2aeb18e33a702c58476528769317fd4db93c284b
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-02-16 14:52:38 -0500

    Static files are served using nginx and django admin page is working well

--- api/config/settings.py
+++ api/config/settings.py
@@ -110,6 +110,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/static/'
+MEDIA_URL = '/static/media/'
+
+MEDIA_ROOT = './vol/media'
+STATIC_ROOT = './vol/static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- api/docker-entrypoint-dev.sh
+++ api/docker-entrypoint-dev.sh
@@ -3,11 +3,11 @@
 echo "Waiting for database to start"
 ./wait-for db:5432
 
-echo "Make migrations"
-python manage.py makemigrations
-
 echo "Migrating database"
 python manage.py migrate
+
+echo "Collect static files"
+python manage.py collectstatic
 
 echo "Running application"
 python manage.py runserver 0.0.0.0:8000
