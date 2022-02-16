36cdc4e2af7396f5553d369da77cc51df781f64b
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-02-16 11:35:01 -0500

    Modified .env

--- api/config/settings.py
+++ api/config/settings.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 import os
+import environ
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(BASE_DIR / ".env")
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'SomeRandomString')
 
