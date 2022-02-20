a78e15e6dcd9276ec533681f5ad49a3afa812170
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-02-19 23:51:07 -0500

    Code added for ssl config

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(barezai.com www.barezai.com)
+domains=(barezai.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="mmmohajer70@gmail.com" # Adding a valid address is strongly recommended
