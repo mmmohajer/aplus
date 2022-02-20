9188f4eb4329631d07b35eb8a15285cfab47fc99
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-02-20 00:48:27 -0500

    Code added for ssl config

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(barezai.com)
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="./nginx/certbot"
 email="mmmohajer70@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
