541d339711cdec0ac808374ddcc5d3e1c3b96250
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-03-25 16:38:18 -0400

    Notification is working pretty well

--- client/src/components/Loading/Loading.js
+++ client/src/components/Loading/Loading.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Div } from "basedesign-iswad";
 
 function Loading() {
   return (
-    <div className="flex flex--jc--center flex--ai--center w-per-100 text-center height-vh-full pos-fix z-10 bgSilver">
-      <div className="flex flex--jc--center flex--ai--center w-px-200 height-px-200 br-rad-per-50 bgBlue textRed">
+    <Div className="flex flex--jc--center flex--ai--center w-per-100 text-center height-vh-full pos-fix z-10 bgSilver">
+      <Div className="flex flex--jc--center flex--ai--center w-px-200 height-px-200 br-rad-per-50 bgBlue textRed">
         Loading
-      </div>
-    </div>
+      </Div>
+    </Div>
   );
 }
 
