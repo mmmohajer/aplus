3be842e7f84f640b05294bdc15420732f31a96a7
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-03-25 20:18:35 -0400

    createReactPageFunction added

--- helpers.sh
+++ helpers.sh
@@ -44,3 +44,28 @@ createReactComponent() {
     
     return 0
 }
+
+createReactPage() {
+    local pageName=$1
+    
+    pageName="$(tr '[:lower:]' '[:upper:]' <<< ${pageName:0:1})${pageName:1}"
+    local addr="client/src/pages/$pageName"
+    mkdir -p "$addr"
+    
+    local jsContext=$(getJsContext $pageName)
+    local indexContext="export { default } from \"./$pageName\";"
+    
+    local innerJsFileAddr="client/src/pages/$pageName/$pageName.js"
+    local innerIndexFileAddr="client/src/pages/$pageName/index.js"
+    local innersassFileAddr="client/src/pages/$pageName/$pageName.module.scss"
+    
+    echo "$jsContext" >> $innerJsFileAddr
+    echo "$indexContext" >> "$innerIndexFileAddr"
+    touch "$innersassFileAddr"
+    
+    echo """Do not forget to add the follwoings to the AppRoutes.js file:
+<Route path=\"/PAGE_URL\" element={<$pageName />}></Route>
+"""
+    
+    return 0
+}
