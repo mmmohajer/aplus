963ea43e0b58f3a26e5a9c97c7100941526336b1
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-04-03 08:06:44 -0400

    Login page created

--- client/src/AppRoutes.js
+++ client/src/AppRoutes.js
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "Pages/Home";
 import Register from "Pages/Register";
+import Login from "Pages/Login";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />}></Route>
       <Route path="/register" element={<Register />}></Route>
+      <Route path="/login" element={<Login />}></Route>
     </Routes>
   );
 }
