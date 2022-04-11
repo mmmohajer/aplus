eafec6798c09f88f13ef3568098104c9f6e9c225
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-04-11 11:35:02 -0400

    Celery timezone added

--- api/config/settings/celery.py
+++ api/config/settings/celery.py
@@ -1,6 +1,9 @@
 from celery.schedules import crontab
+import os
 
 CELERY_BROKER_URL = 'redis://redis:6379/1'
+
+CELERY_TIMEZONE = os.environ.get('API_TIME_ZONE', 'America/Toronto')
 
 CELERY_BEAT_SCHEDULE = {}
 
