edc37c549575990c466bc9008af7bb8675d3f32d
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-04-12 17:17:22 -0400

    Instructions for deployment added

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(iswadco.com www.iswadco.com)
+domains=(iswad.tech www.iswad.tech)
 rsa_key_size=4096
 data_path="./nginx/certbot"
 email="mmmohajer70@gmail.com" # Adding a valid address is strongly recommended
