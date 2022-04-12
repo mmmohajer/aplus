a80e10aecf59771b1532cd223a70287bb689d124
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-04-12 18:57:39 -0400

    test

--- utils/shellScripting/funcs/deploy.sh
+++ utils/shellScripting/funcs/deploy.sh
@@ -5,14 +5,14 @@ buildClient
 git add .
 git commit -m "$commitMsg"
 git push origin master
-local script=$( cat << EOF
-cd /var/www/app;
-echo $sudoPassword | sudo git pull origin master;
-echo $sudoPassword | sudo docker-compose -f docker-compose-prod-ssl.yml down;
-echo $sudoPassword | sudo docker-compose -f docker-compose-prod-ssl.yml up --build -d;
-echo $sudoPassword | sudo docker container prune --force;
-docker image prune -a --force;
-EOF
-)
-ssh $SERVER_ALIAS "$script" 
+# local script=$( cat << EOF
+# cd /var/www/app;
+# echo $sudoPassword | sudo -S git pull origin master;
+# echo $sudoPassword | sudo -S docker-compose -f docker-compose-prod-ssl.yml down;
+# echo $sudoPassword | sudo -S docker-compose -f docker-compose-prod-ssl.yml up --build -d;
+# echo $sudoPassword | sudo -S docker container prune --force;
+# docker image prune -a --force;
+# EOF
+# )
+# ssh $SERVER_ALIAS "$script" 
 }
