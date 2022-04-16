f12e06e36d4061dd9c4f6cd89e22bceba779b773
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-04-16 00:44:12 -0400

    Env variables added

--- api/config/settings/__init__.py
+++ api/config/settings/__init__.py
@@ -4,3 +4,9 @@ from config.settings.constants import *
 
 if USE_CELERY:
     from config.settings.celery import *
+
+if DEBUG:
+    if not os.environ.get('DB_HOST') or os.environ.get('DB_HOST') == 'localhost':
+        MIDDLEWARE.append('corsheaders.middleware.CorsMiddleware')
+        CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
+        INSTALLED_APPS.append('corsheaders')
