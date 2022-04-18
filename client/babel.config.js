cc42463525bb410b338e66a06de25fb1441240d4
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-04-18 13:16:43 -0400

    jest configs added

---
+++ client/babel.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: [
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-proposal-class-properties",
+    "@babel/transform-runtime",
+    "inline-react-svg",
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        alias: {
+          Pages: "./src/pages",
+          Components: "./src/components",
+          Styles: "./src/assets/styles",
+          Images: "./src/assets/images",
+          Reducers: "./src/reducers",
+          Services: "./src/services",
+          Store: "./src/store",
+          Hooks: "./src/hooks",
+          Utils: "./src/utils",
+          Constants: "./src/constants",
+        },
+      },
+    ],
+  ],
+};
