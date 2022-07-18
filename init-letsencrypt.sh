7a08ad061af516f7b00e00f827d860991c9cd41b
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-07-18 13:45:08 -0400

    Domain in env var modified

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(iswad.tech www.iswad.tech)
+domains=(barezai.com www.barezai.com)
 rsa_key_size=4096
 data_path="./nginx/certbot"
 email="mmmohajer70@gmail.com" # Adding a valid address is strongly recommended
