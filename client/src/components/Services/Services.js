1df6ed7bae662a9607f64a3dab7ebba95d4f5a45
unknown <adelkhanin@gmail.com>
2022-07-20 18:44:53 -0700

    services improved

--- client/src/components/Services/Services.js
+++ client/src/components/Services/Services.js
@@ -12,10 +12,14 @@ const Services = () => {
   const language = useSelector((state) => state.language);
 
   const [moveLeft, setMoveLeft] = useState(false);
+  const [moveRight, setMoveRight] = useState(false);
   const [moveToItemWithNum, setMoveToItemWithNum] = useState(1);
 
   return (
     <>
+      <Div className={cx('mb5 h1 text-center', styles.sectionTitle)}>
+        {language === 'en' ? 'Our Services' : 'خدمات ما'}
+      </Div>
       <Div type="flex" hAlign="center" className={cx('show-flex-in-md-lg')}>
         <Div className={cx(styles.container)}>
           {ITEMS.map((item, idx) => (
@@ -33,11 +37,13 @@ const Services = () => {
         <Carousel
           moveLeft={moveLeft}
           setMoveLeft={setMoveLeft}
+          moveRight={moveRight}
+          setMoveRight={setMoveRight}
           moveToItemWithNum={moveToItemWithNum}
           setMoveToItemWithNum={setMoveToItemWithNum}
           className={cx(styles.mobContainer)}>
           {ITEMS.map((item, idx) => (
-            <CarouselItem key={idx} className={cx('w-per-100 mx1')}>
+            <CarouselItem key={idx} className={cx(styles.mobCardItem)}>
               <ServiceCard
                 src={item.src}
                 title={language === 'en' ? item.titleEn : item.titleFa}
@@ -45,14 +51,21 @@ const Services = () => {
               />
             </CarouselItem>
           ))}
-
-          <Div className={cx(styles.carouselButton, styles.carouselButtonLeft)}>
-            <Button onClick={() => setMoveLeft(true)}>Left</Button>
-          </Div>
-          <Div className={cx(styles.carouselButton, styles.carouselButtonRight)}>
-            <Button onClick={() => setMoveToItemWithNum(4)}>Jump To number 4</Button>
-          </Div>
         </Carousel>
+        <Div>
+          <Button
+            className={cx('mx2', styles.carouselButton, styles.carouselButtonLeft)}
+            onClick={() => setMoveLeft(true)}>
+            {'<'}
+          </Button>
+        </Div>
+        <Div>
+          <Button
+            className={cx('mx2', styles.carouselButton, styles.carouselButtonRight)}
+            onClick={() => setMoveRight(true)}>
+            {'>'}
+          </Button>
+        </Div>
       </Div>
     </>
   );
